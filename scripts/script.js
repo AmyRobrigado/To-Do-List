427903ee083462e8f338e74259cf46d570bbd691
Amy Robrigado <amyantanod@gmail.com>
2023-04-13 13:36:19 -0400

    Completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 document.querySelector('#push').onclick = function(){
+    /* If users press button before making inputs */
     if(document.querySelector('#newtask input').value.length == 0)
     {
         alert("Please Enter a Task")
     }
+    /* If user makes an input and presses the button, create a container with the input and a delete button */
     else{
         document.querySelector('#tasks').innerHTML
         += `
@@ -17,5 +19,24 @@ document.querySelector('#push').onclick = function(){
 
                 </div>
         `;
+
+        /* To delete tasks  */
+        var current_tasks = document.querySelectorAll(".delete"); 
+        for(var i = 0; i < current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove(); 
+            }
+        }
     }
+
+    /* Crosses out completed tasks  */
+    var tasks = document.querySelectorAll(".task"); 
+    for(var i = 0; i < tasks.length; i++){
+        tasks[i].onclick = function(){
+            this.classList.toggle('completed'); 
+        }
+    }
+
+    /* Clear input field after each entry */
+    document.querySelector("#newtask input").value = " "; 
 }
